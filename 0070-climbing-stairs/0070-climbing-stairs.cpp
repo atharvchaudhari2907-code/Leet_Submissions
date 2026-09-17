c4cf4d860ac30ee53bf1b390a08805fa1eabee41
Atharv Chaudhari <atharvchaudhari2907@gmail.com>
2026-09-17 21:32:11 +0530

    Time: 0 ms (100%), Space: 7.7 MB (87.91%) - LeetHub - Time: O(n), Space: O(1)

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -17,6 +17,13 @@ public:
         return b;
     }
 };
+/* WHAT I GOT IS:
+IF N=1 OUTPUT IS 1
+IF N=2 OUTPUT IS 2
+IF N=3 OUTPUT IS 3 .... IT'S BASICALLY ADDITION OF PREVIOUS 2 TERMSAS WE DO IN FIBONACCI SERIES
+IF N=4 OUTPUT IS 5 .... IT'S BASICALLY ADDITION OF PREVIOUS 2 TERMSAS WE DO IN FIBONACCI SERIES
+IF N=5 OUTPUT IS 8 .... IT'S BASICALLY ADDITION OF PREVIOUS 2 TERMSAS WE DO IN FIBONACCI SERIES
+SO ON....*/
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
